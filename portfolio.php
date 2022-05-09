e983d802f9eea80d0f783b041ad7bb5324aca19e
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-05-09 18:29:43 +0200

    Auto Save

--- portfolio.php
+++ portfolio.php
@@ -228,6 +228,21 @@
                         </li>
                     </ul>
                 </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/sae104.php"><img src="assets/img-portfolio/exprimer/sae104.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SAE104 - Production audio et vidéo</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
         <section class="competence developper" id="developper">
