7649757342a239f92088f0bca3954117d9b44f76
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-05-12 20:53:17 +0200

    Auto Save

--- portfolio/entreprendre/sae106.php
+++ portfolio/entreprendre/sae106.php
@@ -32,23 +32,57 @@ require("../../debut.php");
                 <h2>SAE105 - Site AC/DC</h2>
             </div>
             <div class="description">
-                <p>Le site de musique AC/DC a été créé durant la SAE105. L'objectif était d'apprendre comment réaliser un tableau de données (ici 100 musiques d'AC/DC). Nous avons également vu comme créer une galerie avec possibilité d'ajouter une image avec un mot de passe. Un système de contact via messagerie a aussi été mis en place pour permettre aux utilisateurs du site de contacter le support pour faire une demande.</p>
+                <p>Un projet est un processus unique composé d’un ensemble d’actions
+                    coordonnées et maîtrisées afin d’atteindre un objectif en respectant des
+                    contraintes de temps, de budget et de ressources. Il existe trois critères pour
+                    définir un projet : le critère temporel, le critère de planification et enfin le ou les
+                    objectifs (livrables). Le but d’un projet est d’atteindre un ou plusieurs objectifs
+                    mais également de résoudre une problématique et de créer de la valeur.<br>
+                    Le projet actuel consiste à venir en aide à une entreprise appelée « Le
+                    Boitelier » situé à Rosières-près-Troyes. Le concept fondamental de cette
+                    entreprise étant l’upcycling ou surcyclage en français. Ce principe consiste à
+                    récupérer des matériaux hors d’usage afin de les revaloriser. L’entreprise est
+                    spécialisée dans les meubles, ils sont donc conçus uniquement à partir de
+                    matériaux de récupération.<br>
+                    Le Boitelier utilise cette méthode de conception car elle permet de réduire
+                    l’impact qu’engendre la destruction de ces déchets sur l’environnement, en leur
+                    donnant une seconde vie. L’upcycling a donc un gros avantage, il permet au
+                    Boitelier de proposer des meubles uniques, en partie grâce aux matériaux utilisés.
+                    En effet, les meubles étant fabriqués à base de matériaux de récupération, les
+                    usures visibles donnent un aspect unique et original, ce qui donne du caractère
+                    aux meubles.<br>
+                    Pour tout achat d’un meuble chez Le Boitelier, vous faites donc l’acquisition
+                    d’un mobilier réalisé avec un savoir-faire artisanal, Made In France, écoresponsable, unique et à un prix attractif.<br>
+                    Le Boitelier est seul, il a une passion pour le travail du bois et de l’acier. Il a
+                    également un grand intérêt pour l’écologie et le développement durable. Le nom
+                    « Le Boitelier » provient de ses origines à savoir de la contraction des mots : bois,
+                    acier, atelier.<br>
+                    Le créateur s’appelle Clément Godereaux, il dispose donc d’un savoir-faire
+                    artisanal. Il est notamment passionné de design, de mobilier et d’architecture.<br>
+                    Clément Godereaux possède un site internet, leboitelier.fr. Ce site permet
+                    de mettre en vente les meubles. Il est ainsi possible de les visualiser et de les
+                    acheter directement en ligne. Il est également possible d’obtenir des meubles sur
+                    mesure.<br>
+                    Le projet consiste donc à subvenir à ses besoins et pour cela de nombreux
+                    objectifs ont été fixés…
+                </p>
             </div>
             <div class="appcri">
                 <div>
-                    <p>• AC4101 | Exploiter de manière autonome un environnement de développement efficace et productif</p>
-                    <p>• AC4102 | Produire des pages Web statiques et fluides utilisant un balisage sémantique efficace</p>
-                    <p>• AC4104 | Mettre en ligne une application Web en utilisant une solution d’hébergement standard</p>
-                    <p>• AC4105 | Modéliser les données et les traitements d’une application Web</p>
+                    <p>• AC5101 | Gérer un projet avec une méthode classique</p>
+                    <p>• AC5102 | Budgétiser un projet et suivre sa rentabilité</p>
+                    <p>• AC5103 | Faire un appel d’offre (cahier des charges, brief, analyse des réponses, choix du prestataire, contractualisation)</p>
                 </div>
             </div>
             <div class="images">
-                <img src="../../assets/img-portfolio/developper/sae105.png" alt="sae105_1" width="400">
-                <img src="../../assets/img-portfolio/developper/sae105_2.png" alt="sae105_2" width="400">
-                <img src="../../assets/img-portfolio/developper/sae105_3.png" alt="sae105_3" width="400">
+                <img src="../../assets/img-portfolio/entreprendre/sae106.png" alt="sae106_1" width="400">
+                <img src="../../assets/img-portfolio/entreprendre/sae106-1.png" alt="sae106_1" width="400">
+                <img src="../../assets/img-portfolio/entreprendre/sae106-2.png" alt="sae106_2" width="400">
+                <img src="../../assets/img-portfolio/entreprendre/sae106-3.png" alt="sae106_3" width="400">
             </div>
             <div class="wherefind">
-                <a href="https://mmi21b12.h205.online/" id="btn" style="margin-top: 0px;" target="_blank">Aller sur le site</a>
+                <a href="../../assets/document/chartesae106.pdf" id="btn" style="margin-top: 0px;" target="_blank">Charte de projet</a>
+                <a href="../../assets/document/plansae106.pdf" id="btn" style="margin-top: 0px;" target="_blank">Plan de projet</a>
             </div>
         </section>
     </main>
