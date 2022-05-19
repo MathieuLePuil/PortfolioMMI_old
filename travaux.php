929811c6b1d372e6617fb9386a4b1ff7d1c6a9ab
MathieuLePuil <mathieu.le-puil@etudiant.univ-reims.fr>
2022-05-19 08:43:35 +0200

    Auto Save

--- travaux.php
+++ travaux.php
@@ -1,11 +1,9 @@
-<!--ssh mmi21b12@mmi21b12.mmi-troyes.fr ./pullrequest.sh-->
-
 <?php
-    require("debut.php");
+require("debut.php");
 ?>
-    <title>MathieuLP | Home</title>
-</head>
-<body>
+    <title>MathieuLP | Portfolio</title>
+    </head>
+    <body>
     <header>
         <div class="logotxt logo">
             <img src="assets/LOGO.png" alt="Logo de l'ATP" id="logohg" width="50" height="50">
@@ -15,12 +13,12 @@
             <label for="check" class="checkbtn">
                 <i class="fa-solid fa-bars"></i>
             </label>
-            <a href="index.php">
+            <a href="https://mathieulp.fr/index.php">
                 <label class="logo">MathieuLP</label></a>
             <ul>
                 <li><a href="index.php" class="anav">Accueil</a></li>
                 <li><a href="travaux.php" class="active anav">Travaux</a></li>
-                <li><a href="portfolio.php" class="anav">Portfolio</a></li>
+                <li><a href="portfolio.php" class="nav">Portfolio</a></li>
                 <li><a href="contact.php" class="anav">Contact</a></li>
             </ul>
         </nav>
@@ -28,13 +26,378 @@
 
     <main>
         <section id="presentation">
-            <h1 class="titre-page">En travaux</h1>
+            <div class="titre-page">
+                <h1>Portfolio</h1>
+            </div>
+            <div class="menu-ue">
+                <nav>
+                    <ul>
+                        <li><a href="#comprendre" class="anav">Semestre 1</a></li>
+                        <li><a href="#semestre2" class="anav">Semestre 2</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <section class="competence comprendre" id="semestre2">
+            <div class="txt-intro">
+                <h2 class="semestre">Semestre 2</h2>
+                <h3>R212 - Intégration</h3>
+            </div>
+            <div class="element-portfolio">
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="https://mmi21b12.mmi-troyes.fr/travaux/r212/sequence1/monopoly.html" target="_blank"><img src="assets/img-travaux/r212/sequence1.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Monopoly</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="https://mmi21b12.mmi-troyes.fr/travaux/r212/sequence2" target="_blank"><img src="assets/img-travaux/r212/sequence2.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Hony Tawk's</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="https://mmi21b12.mmi-troyes.fr/travaux/r212/sequence3/" target="_blank"><img src="assets/img-travaux/r212/sequence3.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Mon parallaxe</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="https://mmi21b12.mmi-troyes.fr/travaux/r212/sequence4" target="_blank"><img src="assets/img-travaux/r212/sequence4.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Calculatrice</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="competence concevoir" id="concevoir">
+            <div class="txt-intro">
+                <h2>Concevoir</h2>
+                <p>Concevoir ou co-concevoir une réponse stratégique pertinente à une problématique complexe</p>
+            </div>
+            <div class="element-portfolio">
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/concevoir/chedsae102.php"><img src="assets/img-portfolio/concevoir/chedsae102.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Charte éditoriale SAE102</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/concevoir/pcsae102.php"><img src="assets/img-portfolio/concevoir/pcsae102.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Plan de communication SAE102</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/concevoir/spotradio.php"><img src="assets/img-portfolio/concevoir/spotradio.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>Spot Radio de sensibilisation</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="competence exprimer" id="exprimer">
+            <div class="txt-intro">
+                <h2>Exprimer</h2>
+                <p>Exprimer un message avec les médias numériques pour informer et communiquer</p>
+            </div>
+            <div class="element-portfolio">
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/wr108.php"><img src="assets/img-portfolio/exprimer/wr108.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR108 - Production graphique</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/wr109.php"><img src="assets/img-portfolio/exprimer/wr109.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR109 - Culture artistique</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/wr208.php"><img src="assets/img-portfolio/exprimer/wr208.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR208 - Production graphique</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/wr209.php"><img src="assets/img-portfolio/exprimer/wr209.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR209 - Culture artistique</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/sae103.php"><img src="assets/img-portfolio/exprimer/sae103.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SAE103 - Design Graphique</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/exprimer/sae104.php"><img src="assets/img-portfolio/exprimer/sae104.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SAE104 - Prod. audio et vidéo</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="competence developper" id="developper">
+            <div class="txt-intro">
+                <h2>Développer</h2>
+                <p>Développer pour le web et les médias numériques</p>
+            </div>
+            <div class="element-portfolio">
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/sae105.php"><img src="assets/img-portfolio/developper/sae105.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SAE105</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/sae203.php"><img src="assets/img-portfolio/developper/sae203.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SAE203</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/scaryshopfr.php"><img src="assets/img-portfolio/developper/scaryshop.fr.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SCARYSHOP.FR</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/wr112.php"><img src="assets/img-portfolio/developper/wr112.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR112</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/wr113.php"><img src="assets/img-portfolio/developper/wr113.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR113</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/wr212.php"><img src="assets/img-portfolio/developper/wr212.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR212</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/developper/wr213.php"><img src="assets/img-portfolio/developper/wr213.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>WR213</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="competence entreprendre" id="entreprendre">
+            <div class="txt-intro">
+                <h2>Entreprendre</h2>
+                <p>Entreprendre dans le secteur du numérique</p>
+            </div>
+            <div class="element-portfolio">
+                <div class="container-a1">
+                    <ul class="caption-style-1">
+                        <li>
+                            <a href="portfolio/entreprendre/sae106.php"><img src="assets/img-portfolio/entreprendre/sae106.png" width="400px"></a>
+                            <div class="caption">
+                                <div class="blur"></div>
+                                <div style="text-align: center;">
+                                    <div class="caption-text">
+                                        <h1>SAE106 - Gestion de projet</h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </section>
     </main>
 
 
 
 <?php
-    require("footer.php");
-    require("fin.php");
+require("footer.php");
+require("fin.php");
 ?>
